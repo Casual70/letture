405b86d4cd94fb960a47ccdfad16d00148dd0d05
casual70 <casual70@gmail.com>
2026-01-02 13:06:17 +0100

    Funzionante

--- custom_riduttori.js
+++ custom_riduttori.js
@@ -1,2 +1,10 @@
-// mi serve uno scritp per collaudare il collegaemento con git
-console.log("Collegamento con git avvenuto con successo!");
+// al cariccamento della pagina controlla che sia presente l'elemento con id "scheda_riduttori"
+// se è presente fammi un allert
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('DOM completamente caricato e analizzato');
+    if (document.getElementById('scheda_riduttori')) {
+        alert('Elemento "scheda_riduttori" trovato!');
+    } else {
+        console.log('Elemento "scheda_riduttori" non trovato.');
+    }
+});
