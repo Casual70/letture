ec572ab8b33e437e74b00e15a9c53da9e254a567
casual70 <casual70@gmail.com>
2026-01-02 12:44:20 +0100

    Collanudo js

--- custom_riduttori.js
+++ custom_riduttori.js
@@ -1,1 +1,8 @@
-
+//usando sono js nativo e non jQuery
+// al cariccamento della pagina controlla che sia presente l'elemento con id "scheda_riduttori"
+// se è presente fammi un allert
+window.addEventListener('load', function() {
+    if (document.getElementById('scheda_riduttori')) {
+        alert('Elemento "scheda_riduttori" trovato!');
+    }
+});
