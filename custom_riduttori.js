0037779246745c67db2bb18cd970fa04e4c56ef8
casual70 <casual70@gmail.com>
2026-01-02 12:46:30 +0100

    collaudo 2

--- custom_riduttori.js
+++ custom_riduttori.js
@@ -1,8 +1,10 @@
 //usando sono js nativo e non jQuery
 // al cariccamento della pagina controlla che sia presente l'elemento con id "scheda_riduttori"
 // se è presente fammi un allert
-window.addEventListener('load', function() {
+document.addEventListener('load', function() {
     if (document.getElementById('scheda_riduttori')) {
         alert('Elemento "scheda_riduttori" trovato!');
+    }else{
+        console.log('Elemento "scheda_riduttori" non trovato.');
     }
 });
